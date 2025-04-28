db888c6593324de6055da69250707f82fed6c81b
m4cbeth <56993541+m4cbeth@users.noreply.github.com>
2025-04-28 17:41:49 -0600

    About to delete godot folder for bug fix

--- Node2D(Card(new)).gd
+++ Node2D(Card(new)).gd
@@ -17,3 +17,35 @@ func _on_area_2d_mouse_entered():
 
 func _on_area_2d_mouse_exited():
 	print('exited')
+
+
+
+func _input(event: InputEvent) -> void:
+	# added "and false" to turn off
+	# need to make this so it only triggers when clicking the card area.
+	# also note there is an animated sprite2d here. Was thinking of using that instaed...
+	# this is fine for now, but also don't delete just move on.
+	# how to if click is on area?
+		# we have that in the card manager "raycast check for card"
+	if is_clicked(event) and false:
+		var side = randi_range(0, 5)
+		$Dice.frame = side
+		match side + 1:
+			1:
+				print('one')
+			2:
+				print('two')
+			3:
+				print('three')
+			4:
+				print('4')
+			5:
+				print(side +1)
+			6:
+				print(6)
+				
+
+
+func is_clicked(e):
+	if e is InputEventMouseButton and e.button_index == MOUSE_BUTTON_LEFT and e.pressed:
+		return true
