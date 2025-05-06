b6713a61aab816764c91fa21cb9d8b27929d7f85
m4cbeth <56993541+m4cbeth@users.noreply.github.com>
2025-05-05 21:20:24 -0600

    Ok, this is now claude fixing claude

--- scripts/state/state_attack.gd
+++ scripts/state/state_attack.gd
@@ -3,7 +3,6 @@ class_name Attack
 
 @onready var sprite = owner.get_node("AnimatedSprite2D")
 @onready var attack_area = owner.get_node("DangerZone")
-
 var attack_timer := 0.0
 var target
 var attack_damage: int
@@ -12,31 +11,38 @@ var attack_damage: int
 @export var attack_target := []
 
 func enter(msg: Dictionary = {}) -> void:
-	sprite.connect("animation_finished", Callable(self, "_on_swing_finish"))
 	if msg.has("target"):
 		target = msg.target
+	if !sprite.animation_finished.is_connected(Callable(self, "_on_animation_finished")):
+		sprite.connect("animation_finished", Callable(self, "_on_animation_finished"))
 	if sprite:
 		sprite.play("Attack")
+		print(sprite.animation)
 	attack_timer = 0.0
 	#perform_attack()
 
 func exit():
-	sprite.disconnect("animation_finished", Callable(self, "_on_swing_finish"))
+	if sprite.animation_finished.is_connected(Callable(self, "_on_animation_finished")):
+		sprite.disconnect("animation_finished", Callable(self, "_on_animation_finished"))
 
-func _on_swing_finish(anim_name):
+func _on_animation_finished(anim_name):
 	if anim_name == "Attack":
 		transition_to("Walk")
+
 func perform_attack():
 	attack_timer = 0.0
 	#if target.has_method("take_damage"): # fuck "target" should be "characterbody" not "vector"
 		#target.take_damage(attack_damage)
+	for body in attack_area.get_overlapping_bodies():
+		if body.is_in_group("bad_guys"):
+			body.take_damage()
 	if sprite:
 		sprite.play("Attack")
-		
 
 func update(delta):
 	if not is_instance_valid(target) or entity.global_position.distance_to(target) > 100:
 		transition_to("Walk")
+		return
 	attack_timer += delta
 	if attack_timer > attack_cooldown:
 		perform_attack()
@@ -47,8 +53,3 @@ func physics_update(_delta):
 #helper function so can call trans in state
 func transition_to(state_name: String, msg: Dictionary = {}) -> void:
 	state_machine.transition_to(state_name, msg)
-
-#func do_damage():
-	#for body in attack_area.get_overlapping_bodies():
-		#if body.is_in_group("bad_guys"):
-			#body.take_damage()
